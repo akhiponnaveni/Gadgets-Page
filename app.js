8afc03babf7ea3732245d51d044a1f630306fde2
akhi <akhiponnaveni09@gmail.com>
2023-02-28 07:42:22 +0000

    send file

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+app.get("/gadgets", (requist, response) => {
+  response.sendFile("gadgets.html", { root: __dirname });
+});
+module.exports = app;
